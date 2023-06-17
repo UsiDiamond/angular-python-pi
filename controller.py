9a9c38dbc26463ea7e6ea2c1c5d1d728bf6d7c32
Usi Diamond <UsiDiamond@proton.me>
2023-06-17 14:45:11 -0400

    working with cors

--- controller.py
+++ controller.py
@@ -3,6 +3,7 @@ def hello(environ, start_response):
     data = b"Hello, World!\n"
     start_response("200 OK", [
         ("Content-Type", "text/plain"),
+        ('Access-Control-Allow-Origin', '*'),
         ("Content-Length", str(len(data)))
     ])
     return iter([data])
